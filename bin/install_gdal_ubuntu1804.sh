e583d8ea1745c21dd14b3dea77bbce94d509c491
Fei Zhang <fei.zhang@ga.gov.au>
2020-01-31 15:31:32 +1100

    add a script to install gdal bin and python lib in ubuntu

---
+++ bin/install_gdal_ubuntu1804.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# See this article
+#      https://mothergeo-py.readthedocs.io/en/latest/development/how-to/gdal-ubuntu-pkg.html
+
+# install gdal and python-wrapper are not that straight forward.
+# In ubuntu, you can use the repository ubuntugis
+
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get update
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+
+gdalver=`ogrinfo --version`
+pip install GDAL==$gdalver
+
+#pip install GDAL==<GDAL VERSION FROM OGRINFO>
+
