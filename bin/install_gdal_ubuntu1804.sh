c1c8784a124a8434119940c11dced1bc62716d5f
Fei Zhang <fei.zhang@ga.gov.au>
2020-01-31 15:39:41 +1100

    update bin/install_gdal_ubuntu1804.sh

--- bin/install_gdal_ubuntu1804.sh
+++ bin/install_gdal_ubuntu1804.sh
@@ -10,6 +10,10 @@ sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
 sudo apt-get update
 sudo apt-get install gdal-bin
 sudo apt-get install libgdal-dev
+
+
+# Now in a suitable virtual env,  you can do the pip install as follows: 
+
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
 
