06fc41d61fc6629ac0342fd942dfdc6bf930b180
Bren Moushall <bmoush@gmail.com>
2020-04-20 16:33:46 +1000

    Update spike detection
    - Add plot of all stations
    - Add saving of all plots to pdf
      - Plotting code needs to be re-written to get them side-by-side
    - Handle cases where no peaks/troughs are found
    - Add minimum depth parameter
    - Add minimum prominence parameter

--- examples/scripts/spike_detection.py
+++ examples/scripts/spike_detection.py
@@ -2,10 +2,13 @@ import sys
 import os
 
 from scipy.signal import find_peaks, peak_prominences, peak_widths
+from matplotlib.pyplot import cm
+from matplotlib.axes._axes import _log as _mpl_logger
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+_mpl_logger.setLevel('ERROR')
 
 def load_data(station_dir):
     d = np.loadtxt(os.path.join(station_dir, 'interface_depth_hist.txt'))
@@ -14,59 +17,103 @@ def load_data(station_dir):
     return depths, res
 
 
-def peak_info(res, depth, max_depth=500, troughs=False):
+def peak_info(res, depth, min_prominence=0, min_depth=0, max_depth=500, troughs=False):
     """
     """
-    depth_limit = depth <= max_depth
+    depth_limit = np.logical_and(min_depth <= depth, depth <= max_depth)
     depth = depth[depth_limit]
     res = res[depth_limit]
     if troughs:
-        peaks = find_peaks(-res)[0]
+        peaks = find_peaks(-res, prominence=min_prominence)[0]
     else:
-        peaks = find_peaks(res)[0]
+        peaks = find_peaks(res, prominence=min_prominence)[0]
+
+    if len(peaks) == 0:
+        return (())
+
     peak_depths, peak_res = depth[peaks], res[peaks]
+
     if troughs:
         peak_prom = peak_prominences(-res, peaks)[0]
         peak_width = peak_widths(-res, peaks)[0]
     else:
         peak_prom = peak_prominences(res, peaks)[0]
         peak_width = peak_widths(res, peaks)[0]
-    peak_info = tuple(reversed(sorted(zip(peak_depths, peak_res, peak_prom, peak_width), key=lambda x: x[2])))
+    peak_info = tuple(reversed(sorted(zip(peak_depths, peak_res, peak_prom, peak_width), 
+                               key=lambda x: x[2])))
     return peak_info
 
 
-def plot(depth, res, peaks, troughs=None, max_depth=500, savepath='/tmp/peaks.png'):
+def plot(station_name, depth, res, peaks, troughs=None, min_depth=0, max_depth=500,
+         savepath='/tmp/peaks.png'):
     fig, ax = plt.subplots(figsize=(5, 10))
+    fig.suptitle(f"Potential Layer Changes ({station_name})")
     xlim = [-10, np.max(res)]
-    ylim = [max_depth, 0]
+    ylim = [max_depth, min_depth]
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
+    ax.set_ylabel("Depth (m)")
+    ax.set_xlabel("Resistivity")
+    plt.xticks(rotation=45)
 
-    ax.plot(res, depth, 'b', lw=0.5)
+    ax.plot(res, depth, c='b', lw=0.5)
 
-    peak_depths, peak_res, _, _ = zip(*peaks)
-    ax.scatter(peak_res, peak_depths, c='b')
+    if all(len(p) == 0 for p in peaks):
+        pass
+    else:
+        peak_depths, peak_res, _, _ = zip(*peaks)
+    ax.scatter(peak_res, peak_depths, c='b', lw=0.5)
     if troughs is not None:
-        trough_depths, trough_res, _, _ = zip(*troughs)
-        ax.scatter(trough_res, trough_depths, c='r')
+        if all(len(p) == 0 for p in troughs):
+            pass
+        else:
+            trough_depths, trough_res, _, _ = zip(*troughs)
+            ax.scatter(trough_res, trough_depths, c='b', lw=0.5)
+    fig.savefig(savepath)
+    return fig
+
+
+def all_plot(station_data, min_depth=0, max_depth=500, savepath='/tmp/all_peaks.png'):
+    fig, ax = plt.subplots(figsize=(5, 10))
+    fig.suptitle("Potential Layer Changes (All Stations)")
+
+    color_iterator = iter(cm.rainbow(np.linspace(0, 1, len(station_data))))
+    highest_res = 0
+    for sd in station_data:
+        name, depth, res, peaks, troughs = sd
+        highest_res = max(np.max(res), highest_res)
+
+        c = next(color_iterator)
+        ax.plot(res, depth, label=name, lw=0.5, c=c)
+       
+        if all(len(p) == 0 for p in peaks):
+            pass
+        else:
+            peak_depths, peak_res, _, _ = zip(*peaks)
+            ax.scatter(peak_res, peak_depths, c=c)
+
+        if all(len(t) == 0 for t in troughs):
+            pass
+        else:
+            trough_depths, trough_res, _, _ = zip(*troughs)
+            ax.scatter(trough_res, trough_depths, c=c)
+
+    ax.legend()
+    ylim = [max_depth, min_depth]
+    xlim = [-10, highest_res]
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_ylabel("Depth (m)")
+    ax.set_xlabel("Resistivity")
+    plt.xticks(rotation=45)
+
     fig.savefig(savepath)
 
 
 def output_csv(peaks, savepath='/tmp/peaks.csv'):
-    depths, res, prom, width = zip(*peaks)
-    df = pd.DataFrame({'depth': depths, 'resistivity': res, 'prominence': prom, 'width': width})
+    if all(len(p) == 0 for p in peaks):
+        df = pd.DataFrame({'no_peaks_found': [0]})
+    else:
+        depths, res, prom, width = zip(*peaks)
+        df = pd.DataFrame({'depth': depths, 'resistivity': res, 'prominence': prom, 'width': width})
     df.to_csv(savepath)
-
-if __name__ == '__main__':
-    """
-    argv[1]: Path to rjmcmcmt output station dir
-    argv[2]: Max depth in metres (default 500)
-    """
-    file_path = sys.argv[1]
-    max_depth = float(sys.argv[2])
-    depths, res = load_data(file_path)
-    peaks = peak_info(res, depths, max_depth)
-    troughs = peak_info(res, depths, max_depth, troughs=True)
-    output_csv(peaks)
-    output_csv(troughs, savepath='/tmp/troughs.csv')
-    plot(depths, res, peaks, troughs)
--- examples/scripts/spike_detection_script.py
+++ examples/scripts/spike_detection_script.py
@@ -6,20 +6,50 @@ script to visualise rjmcmcmt output
 import os
 import spike_detection as sd
 
+from matplotlib.backends.backend_pdf import PdfPages
+
 resultsdir = '/home/bren/rjmcmcmt/examples/SouthernThomson/output'
 outputdir = '/tmp/test_output'
-if not os._exists(outputdir):
+if not os.path.exists(outputdir):
     os.mkdir(outputdir)
 
+# Parameters #
+
+# Depth range to search for peaks/troughs
+min_depth = 100
 max_depth = 500
 
+# The minimum prominence of the peak/trough to be included in results
+# For an explanation of how prominence is calculated:
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.peak_prominences.html#scipy-signal-peak-prominences
+min_prominence = 100000
+
+# End parameters #
+
+station_data = []
+figures = []
 for path in os.listdir(resultsdir):
     station_name = path
     station_dir = os.path.join(resultsdir, station_name)
     depths, res = sd.load_data(station_dir)
-    peaks = sd.peak_info(res, depths, max_depth)
-    troughs = sd.peak_info(res, depths, max_depth, troughs=True)
-    sd.plot(depths, res, peaks, troughs, savepath=os.path.join(outputdir, station_name + '.png'))
+    peaks = sd.peak_info(res, depths, min_prominence, min_depth, max_depth)
+    troughs = sd.peak_info(res, depths, min_prominence, min_depth, max_depth, troughs=True)
+    figures.append(sd.plot(station_name, depths, res, peaks, troughs, min_depth, max_depth, 
+                   savepath=os.path.join(outputdir, station_name + '.png')))
     sd.output_csv(peaks, savepath=os.path.join(outputdir, station_name + '_peaks.csv'))
     sd.output_csv(troughs, savepath=os.path.join(outputdir, station_name + '_troughs.csv'))
+    for p in peaks:
+        print(f"Potential layer change in {station_name} at depth {p[0]}m with res {p[1]}")
+    for t in troughs:
+        print(f"Potential layer change in {station_name} at depth {t[0]}m with res {t[1]}")
+    station_data.append((station_name, depths, res, peaks, troughs))
 
+figures.append(sd.all_plot(station_data, min_depth, max_depth, 
+                           savepath=os.path.join(outputdir, 'all_peaks.png')))
+
+pdf = PdfPages(os.path.join(outputdir, 'all_plots.pdf'))
+for fig in figures:
+    pdf.savefig(fig)
+pdf.close()
+
+print(f"Complete! Outputs are located in '{outputdir}'")
