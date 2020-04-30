fcb0c39f31f4b90af7aeb4f2f2922b705ed25543
Bren Moushall <bmoush@gmail.com>
2020-04-30 13:13:53 +1000

    Selecting zones by mag range + basic zone plot
    - Can specify a range of magnitudes (e.g 1 = 0 : 1, 1 : 10...,
      2 = 0 : 10, 10 : 1000) for selecting zones. Zones are decided based
      on the magnitude range their average resistivity falls into.
    - Rudimentary plot of selected zones. Need a better solution with
      clearly labelled zones (this is difficult when there are a large
      number of unique zones)

--- mtpy/modeling/modem/zones.py
+++ mtpy/modeling/modem/zones.py
@@ -9,9 +9,12 @@ CreationDate: 28-04-2020 10:51:34 AEST
 Developer: brenainn.moushall@ga.gov.au
 """
 import sys
+import math
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.cm as mpl_cm
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from scipy.ndimage import label
 
@@ -30,8 +33,32 @@ def _load_model(model_file):
     return model
 
 
-def find_zones(model_file, x_pad=None, y_pad=None, z_pad=None,
-               depths=None):
+def _meanshift_cluster(res):
+    """
+    Using MeanShift clustering to label cells
+    """
+    # TODO: instead of averaging res across slices, we could provide
+    # each slice as a feature
+    # Need to transpose the data into shape (X*Y, 1) for inputting
+    # to mean shift clusterer.
+    X = res.flatten().reshape(-1, 1)
+    # See https://scikit-learn.org/stable/modules/generated/sklearn.cluster.MeanShift.html
+    bw = estimate_bandwidth(X, quantile=0.3, random_state=1)
+    msc = MeanShift(bw)
+    labels = msc.fit_predict(X)
+    labels = np.squeeze(labels.reshape(res.shape))
+    return labels
+
+
+def _magnitude(res, mag_range):
+    """
+    Labels cells according to the magnitude of their resistivity
+    """
+    return np.log10(res) // mag_range
+
+
+def find_zones(model_file, x_pad=None, y_pad=None, z_pad=None, depths=None, method='cluster',
+               magnitude_range=None, value_ranges=None):
     if isinstance(depths, tuple) or isinstance(depths, list):
         if depths[1] <= depths[0]:
             raise ValueError("Provided depth range is invalid. Max depth ({}) must be less than "
@@ -41,9 +68,13 @@ def find_zones(model_file, x_pad=None, y_pad=None, z_pad=None,
                         "(min, max), a single depth as an integer or float, or None to get all "
                         "depths in the model. Provided 'depths' was of type {}".format(type(depths)))
 
+    if method == 'magnitude' and magnitude_range is None:
+        raise TypeError("magnitude_range must be provided when using 'magnitude' method")
+    elif method == 'value' and value_ranges is None:
+        raise TypeError("value_ranges must be provided when using 'value_ranges' method")
+
     model = _load_model(model_file)
 
-    # Get paddings if not provided
     x_pad = model.pad_east if x_pad is None else x_pad
     y_pad = model.pad_north if y_pad is None else y_pad
     z_pad = model.pad_z if z_pad is None else z_pad
@@ -61,26 +92,30 @@ def find_zones(model_file, x_pad=None, y_pad=None, z_pad=None,
         inds = list(get_depth_indices(grid_z, depths))
 
     # All depths, depth range, single depth, polygons containing cells
-    # TODO: handle finding zones by averaging across multiple depths
     # TODO: region finding methods - by magnitudes, by user defined ranges
     # TODO: plot found zones
+    if not inds:
+        raise ValueError("No indices could be found in model depth grid for the depths provided.")
     if len(inds) == 1:
         # Dealing with a single depth - don't take mean
-        # Log10 method - regions grouped by magnitude
-        # Clustering method
-        res_sd = res[:, :, inds]
-        # Need to transpose the data into shape (X*Y, 1) for inputting
-        # to mean shift clusterer.
-        X = res_sd.flatten().reshape(-1, 1)
-        # See https://scikit-learn.org/stable/modules/generated/sklearn.cluster.MeanShift.html
-        bw = estimate_bandwidth(X, quantile=0.3, random_state=1)
-        msc = MeanShift(bw)
-        labels = msc.fit_predict(X)
-        labels = np.squeeze(labels.reshape(res_sd.shape))
+        res_sd = res[:, :, inds]  # res for selected depths
+    else:
+        # Dealing with multiple depths - take mean across depths
+        res_sd = np.mean(res[:, :, inds], axis=2)  # res for selected depths
+
+    if method == 'cluster':
+        labels = _meanshift_cluster(res_sd)
+    elif method == 'magnitude':
+        labels = _magnitude(res_sd, magnitude_range)
 
     # Find contiguous groups of labels - these are the zones
     groups = _contiguous_element_grouper(labels)
     print(f"Unique regions found: {len(np.unique(groups))}")
+
+    fig, ax = plt.subplots()
+    norm = matplotlib.colors.BoundaryNorm(boundaries=np.unique(groups), ncolors=len(np.unique(groups)))
+    ax.imshow(groups, cmap=mpl_cm.gist_ncar, norm=norm)
+    fig.savefig('/tmp/zones.png')
 
     # Get X, Y indices for each region
     zones = []
@@ -109,7 +144,7 @@ def _contiguous_element_grouper(labels):
     result = np.zeros_like(labels)
     for l in flat_labels:
         labelled, num_features = label(labels == l)
-        result += labelled + offset * (labelled > 0)
+        result += labelled + offset + (labelled > 0)
         offset += 1
     return result
 
@@ -129,11 +164,12 @@ def plot_zone(zone_mean_res, model_depths, zone_name, min_depth=None, max_depth=
         ax.set_xlabel("Resistivity (no scaling)")
     ax.plot(zone_mean_res, model_depths)
     fig.savefig(f'/tmp/{zone_name}.png')
-    fig.close()
+    plt.close(fig)
 
 
+# Test code
 if __name__ == '__main__':
-    zr, depths = find_zones(sys.argv[1], depths=500)
+    zr, depths = find_zones(sys.argv[1], depths=(0, 500), method='magnitude', magnitude_range=2)
     for zone_id, zone in enumerate(zr):
         plot_zone(zone, depths, f'Zone {zone_id}', 10, 10000, 'log')
 
