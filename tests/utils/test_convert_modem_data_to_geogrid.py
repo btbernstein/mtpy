913363d6a57effea21000860b44eefadef1d7d9a
Bren Moushall <bmoush@gmail.com>
2020-06-24 16:51:40 +1000

    Fix test (refactored functions)

--- tests/utils/test_convert_modem_data_to_geogrid.py
+++ tests/utils/test_convert_modem_data_to_geogrid.py
@@ -5,23 +5,24 @@ import pytest
 import numpy as np
 
 from mtpy.utils import convert_modem_data_to_geogrid as conv
+from mtpy.utils import modem_utils as mu
 
 
 def test_strip_padding():
     grid_axis = [-2., -1., 0., 1., 2.]
-    test = conv._strip_padding(grid_axis, 1)
+    test = mu.strip_padding(grid_axis, 1)
     expected = [-1., 0., 1.]
     np.testing.assert_array_equal(test, expected)
 
     # For 0 padding case, the array should come back untouched.
-    test = conv._strip_padding(grid_axis, 0)
+    test = mu.strip_padding(grid_axis, 0)
     expected = grid_axis
     np.testing.assert_array_equal(test, expected)
 
     # When keep_start=True, padding should only be removed from the
     #  end. This is used for stripping padding from Z-axis (only has
     #  padding cells on the bottom).
-    test = conv._strip_padding(test, 1, keep_start=True)
+    test = mu.strip_padding(test, 1, keep_start=True)
     expected = [-2., -1., 0., 1.]
     np.testing.assert_array_equal(test, expected)
 
@@ -31,7 +32,7 @@ def test_gdal_origin():
     grid_north = [-1.5, 0., 1.5]
     # Center of the survey site, puts the grid into reference
     center_point = 20., 150.
-    test = conv._get_gdal_origin(grid_east, 1., center_point[1],
+    test = mu.get_gdal_origin(grid_east, 1., center_point[1],
                                  grid_north, 1.5, center_point[0])
     # Origin should be upper-left, so western-most point shifted 1/2
     #  cell west and northern-most point shifted 1/2 cell north.
@@ -65,7 +66,7 @@ def test_strip_resgrid():
     ])
     # Transpose to get [[Y], [X], [Z]]
     resgrid = resgrid.T
-    test = conv._strip_resgrid(resgrid, 1, 1, 1)
+    test = mu.strip_resgrid(resgrid, 1, 1, 1)
     # We're padding one cell off either side of X and Y, and one cell
     #  off the end of Z. So we're left with the [150.] array in middle
     #  of the top row, with two elements from either side removed.
@@ -77,18 +78,18 @@ def test_strip_resgrid():
 def test_get_depth_indicies():
     z_cells = np.asarray([0, 100, 500, 1000, 10000])
 
-    assert conv._get_depth_indicies(z_cells, [49]) == set([0])
-    assert conv._get_depth_indicies(z_cells, [500]) == set([2])
+    assert mu.get_depth_indices(z_cells, [49]) == set([0])
+    assert mu.get_depth_indices(z_cells, [500]) == set([2])
 
     # Should round up to nearest depth
-    assert conv._get_depth_indicies(z_cells, [50]) == set([1])
+    assert mu.get_depth_indices(z_cells, [50]) == set([1])
 
     # If two provided depths are closest to same index, only return
     #  that index (by returning a set)
-    assert conv._get_depth_indicies(z_cells, [50, 51]) == set([1])
+    assert mu.get_depth_indices(z_cells, [50, 51]) == set([1])
 
     # If no depths are provided, return every index in a set
-    assert conv._get_depth_indicies(z_cells, []) == {0, 1, 2, 3, 4}
+    assert mu.get_depth_indices(z_cells, []) == {0, 1, 2, 3, 4}
 
 
 def test_interpolate_depth_slice():
@@ -134,11 +135,11 @@ def test_rotate_geotransform():
     gt = [origin_x, pixel_width, 0, origin_y, 0, pixel_height]
 
     # Rotate about the upper-left
-    test = conv._rotate_transform(gt, angle, origin_x, origin_y)
+    test = mu.rotate_transform(gt, angle, origin_x, origin_y)
     expected = [gt[0], 3.061616997868383e-16, -5., gt[3], 5., 3.061616997868383e-16]
 
     assert test == expected
 
     # Rotate about a center point of (0., 0.)
-    test = conv._rotate_transform(gt, angle, 0., 0.)
+    test = mu.rotate_transform(gt, angle, 0., 0.)
     expected = [100.0, 3.061616997868383e-16, -5., -49.99999999999999, 5., 3.061616997868383e-16]
